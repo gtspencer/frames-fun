03b02ca6f695377f928e3d2242f3c726d9ca92fe
Spencer <spencerobsitnik@gmail.com>
2024-04-07 21:18:19 -0400

    simplify

--- app/hattip/route.tsx
+++ app/hattip/route.tsx
@@ -4,42 +4,6 @@ import { ImageResponse } from 'next/og'
 
 export const runtime = 'edge'
 
-function extractNumbersAndBoolean(inputString: string) {
-    let number = '';
-    let boolValue = '';
-    let foundNumber = false;
-    let foundBoolValue = false;
-
-    // Iterate through each character of the input string
-    for (let i = 0; i < inputString.length; i++) {
-        const char = inputString[i];
-
-        // If the character is a digit and number hasn't been found yet
-        if (!foundNumber) {
-            number += char;
-        }
-        // If the character is 'true' or 'false' and boolean value hasn't been found yet
-        else if ((char === 't' || char === 'f') && !foundBoolValue) {
-            if (inputString.substring(i, i + 4) === 'true') {
-                boolValue = 'true';
-                foundBoolValue = true;
-                i += 3; // Skip ahead to avoid rechecking 'true' characters
-            } else if (inputString.substring(i, i + 5) === 'false') {
-                boolValue = 'false';
-                foundBoolValue = true;
-                i += 4; // Skip ahead to avoid rechecking 'false' characters
-            }
-        }
-        // If both number and boolean value have been found, break the loop
-        if (foundNumber && foundBoolValue) {
-            break;
-        }
-    }
-
-    // Return the extracted values
-    return { number, boolValue };
-}
-
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
 
@@ -60,14 +24,10 @@ export async function GET(request: Request) {
     ).then((res) => res.arrayBuffer())
 
     let responseMessage = ""
-    if (tips == "1" && !(alreadyTippedToday == 'true' || alreadyTippedToday === 'true')) {
-        responseMessage = "Your first hat tip!  Come back tomorrow for more."
+    if (alreadyTippedToday == 'true' || alreadyTippedToday === 'true') {
+        responseMessage = `You have already tipped your hat today.\nTotal hat tips: ${tips}\nCome back tomorrow for more`
     } else {
-        if (alreadyTippedToday == 'true' || alreadyTippedToday === 'true') {
-            responseMessage = `You have already tipped your hat today.\nTotal hat tips: ${tips}\nCome back tomorrow for more`
-        } else {
-            responseMessage = `Hat tipped!\nTotal hat tips: ${tips}\nCome back tomorrow for more`
-        }
+        responseMessage = `Hat tipped!\nTotal hat tips: ${tips}\nCome back tomorrow for more`
     }
 
     return new ImageResponse(
